3fcd5530575cd1f17aea7979b638ec80929fb40b
gabriel04alves <gabriel52.alves@gmail.com>
2022-06-16 16:20:02 -0300

    commit

---
+++ init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAjpNlWtDUmia_PCPDqyiIEhX-7Lj17p3I",
+    authDomain: "soateofigma.firebaseapp.com",
+    projectId: "soateofigma",
+    storageBucket: "soateofigma.appspot.com",
+    messagingSenderId: "556930160292",
+    appId: "1:556930160292:web:ff3197df432c4f38f3f831"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
